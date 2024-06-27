a6f55e848a5d87fa4ba390160b1f3c8af208503f
fdmora <fdmora@gmail.com>
2024-06-27 14:11:35 -0600

    BestTimetoBuyandSellStockII

---
+++ BestTimetoBuyandSellStockII/BestTimetoBuyandSellStock II/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+    int[] price = { 7, 1, 5, 3, 6, 4 };
+    int vmaxProfit = 0;
+    vmaxProfit = MaxProfit(price);
+    Console.WriteLine(vmaxProfit);
+Console.ReadLine();
+int MaxProfit(int[] prices)
+    {
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.Length; i++)
+        {
+            if (prices[i] > prices[i - 1])
+            {
+                maxProfit += prices[i] - prices[i - 1];
+            }
+        }
+
+        return maxProfit;
+    }
+
